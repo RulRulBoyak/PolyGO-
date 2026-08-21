72de90f5d1fec3cab316541d9d9cf071815276a5
kesatriahitam7447 <ignfebian@gmail.com>
2026-08-21 11:44:01 +0800

    Minor Chat Activity Implementation

--- app/src/main/java/com/poliku/polygoplus/ChatActivity.java
+++ app/src/main/java/com/poliku/polygoplus/ChatActivity.java
@@ -7,6 +7,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -30,6 +33,12 @@ public class ChatActivity extends AppCompatActivity {
         adapter = new ChatMessageAdapter(AppDataStore.userName(this));
         messageList.setAdapter(adapter);
         input = findViewById(R.id.etMessage);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.chat_main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         findViewById(R.id.btnBack).setOnClickListener(v -> finish());
         findViewById(R.id.btnSend).setOnClickListener(v -> sendMessage());
